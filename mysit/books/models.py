bf937abc0d5f687c19988bb16b2b0368bcd7688e
LevTian <tyln2013@163.com>
2018-06-19 10:22:40 +0800

    update admin

--- mysit/books/models.py
+++ mysit/books/models.py
@@ -14,16 +14,16 @@ class Publisher(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, verbose_name='e-mail')
 
     def __str__(self):
-        return self.first_name
+        return '%s %s' % (self.first_name, self.last_name)
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE) #2.0
-    publication_date = models.DateField()
+    publication_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.title
